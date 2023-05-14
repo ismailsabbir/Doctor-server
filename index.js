4db52f9a5f7ea7f792bb7944aa7a3214f6bcddd1
Your Name <you@example.com>
2023-05-14 11:51:02 +0600

    env file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+require('dotenv').config();
 const { MongoClient, ObjectId } = require('mongodb');
 const cors = require('cors')
 const app = express()
@@ -7,6 +8,7 @@ app.use(cors())
 app.use(express.json())
 // password:koTKjunMt4Hfgmd3
 // dbname:doctor 
+// const uri = "mongodb+srv://${process.env.db-name}:${process.env.password}@cluster0.3w5podw.mongodb.net/?retryWrites=true&w=majority";
 const uri = "mongodb+srv://doctor:koTKjunMt4Hfgmd3@cluster0.3w5podw.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri);
 async function run() {
